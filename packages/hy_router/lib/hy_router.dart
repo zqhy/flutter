668da1281970fcfdaa4f7fc931dfcd2c6dda2fc5
邵东华 <708990353@qq.com>
2020-10-29 09:14:50 +0800

    增加`hy_router`

---
+++ packages/hy_router/lib/hy_router.dart
@@ -0,0 +1,5 @@
+library hy_router;
+
+export 'src/navigate_result.dart';
+export 'src/page_router.dart';
+export 'src/return_router_info.dart';
---
+++ packages/hy_router/lib/src/navigate_result.dart
@@ -0,0 +1,3 @@
+enum NavigateResult {
+  canceled, ok
+}
