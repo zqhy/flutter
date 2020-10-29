2c07aaa62ced568e095f26e9d22886f486df2ab9
innwin <innwin@live.com>
2020-10-29 09:18:47 +0800

    增加`hy_rx`

---
+++ packages/hy_rx/lib/src/extensions/rx_stream_ext.dart
@@ -0,0 +1,19 @@
+import 'package:rxdart/rxdart.dart';
+
+extension RxStreamExt<T> on Stream<T> {
+  BehaviorSubject<T> toBehaviorSubject() {
+    final subject = BehaviorSubject<T>();
+    listen(subject.add, onError: subject.addError);
+    return subject;
+  }
+
+  BehaviorSubject<T> toBehaviorSubjectSeeded(T seedValue, {
+    void Function() onListen,
+    void Function() onCancel,
+    bool sync = false,
+  }) {
+    final subject = BehaviorSubject<T>.seeded(seedValue, onListen: onListen, onCancel: onCancel, sync: sync);
+    listen(subject.add, onError: subject.addError);
+    return subject;
+  }
+}
