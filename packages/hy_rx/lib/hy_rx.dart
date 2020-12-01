f4ad8f0f03f10413ba38d98a0cc3c0eb912d0e24
邵东华 <708990353@qq.com>
2020-12-01 16:13:27 +0800

    `hy_rx`中增加倒计时逻辑

--- packages/hy_rx/lib/hy_rx.dart
+++ packages/hy_rx/lib/hy_rx.dart
@@ -1,5 +1,6 @@
 library hy_rx;
 
+export 'src/extensions/behavior_subject_ext.dart';
 export 'src/extensions/rx_bool_ext.dart';
 export 'src/extensions/rx_stream_ext.dart';
 export 'src/extensions/value_stream_ext.dart';
---
+++ packages/hy_rx/lib/src/extensions/behavior_subject_ext.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+
+extension BehaviorSubjectExt on BehaviorSubject<int> {
+
+  static StreamSubscription _countdownSubscription;
+  void countdown(int seconds) {
+    _countdownSubscription?.cancel();
+    _countdownSubscription = Stream.periodic(Duration(seconds: 1), (x) => seconds - x - 1).takeWhile((ticks) => ticks >= 0).startWith(seconds).listen((ticks) {
+      if (!isClosed) {
+        add(ticks);
+      }
+    }, onError: (error) => _countdownSubscription?.cancel(),
+        onDone: () => _countdownSubscription?.cancel());
+  }
+}
