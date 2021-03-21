e2b8d2a49c8b8d88c3fe678b9031c7098a5527ff
innwin <innwin@live.com>
2021-03-21 11:53:25 +0800

    `hy_channel_info`添加 Null safety

--- packages/hy_logger/lib/src/logger.dart
+++ packages/hy_logger/lib/src/logger.dart
@@ -6,27 +6,27 @@ class Logger implements ILogger {
   final _logger = logger.Logger();
 
   @override
-  void v(message, [dynamic error, StackTrace stackTrace]) {
+  void v(message, [dynamic error, StackTrace? stackTrace]) {
     _logger.v(message, error, stackTrace);
   }
 
   @override
-  void d(message, [dynamic error, StackTrace stackTrace]) {
+  void d(message, [dynamic error, StackTrace? stackTrace]) {
     _logger.d(message, error, stackTrace);
   }
 
   @override
-  void i(message, [dynamic error, StackTrace stackTrace]) {
+  void i(message, [dynamic error, StackTrace? stackTrace]) {
     _logger.i(message, error, stackTrace);
   }
 
   @override
-  void w(message, [dynamic error, StackTrace stackTrace]) {
+  void w(message, [dynamic error, StackTrace? stackTrace]) {
     _logger.w(message, error, stackTrace);
   }
 
   @override
-  void e(message, [dynamic error, StackTrace stackTrace]) {
+  void e(message, [dynamic error, StackTrace? stackTrace]) {
     _logger.e(message, error, stackTrace);
   }
 }
