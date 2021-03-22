26c3c1508207e98133c4a25a8cb207e9b1515d31
innwin <innwin@live.com>
2021-03-22 09:12:49 +0800

    `hy_protobuf`添加 Null safety

--- packages/hy_protobuf/lib/hy_protobuf.dart
+++ packages/hy_protobuf/lib/hy_protobuf.dart
@@ -1,4 +1,7 @@
 library hy_protobuf;
 
+// https://github.com/dart-lang/protobuf/tree/master/protoc_plugin/test/protos/google/protobuf
+// https://github.com/dart-lang/protobuf
+
 export 'generated/protos/google/protobuf/timestamp.pb.dart';
 export 'generated/protos/google/protobuf/wrappers.pb.dart';
