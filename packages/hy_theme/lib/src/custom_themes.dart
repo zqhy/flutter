369aaa1159c6cf525db94f2bc5b487e36ddf5018
innwin <innwin@live.com>
2021-03-22 15:08:35 +0800

    `hy_theme`添加 Null safety

--- packages/hy_theme/lib/src/custom_themes.dart
+++ packages/hy_theme/lib/src/custom_themes.dart
@@ -8,21 +8,21 @@ import 'storage_by_type.dart';
 /// Storage for custom themes data.
 class CustomThemes extends InheritedWidget {
   /// Obtains the nearest CustomThemes.
-  static CustomThemes _of(BuildContext context) {
+  static CustomThemes? _of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<CustomThemes>();
   }
 
   /// Obtains the theme data of given type from the nearest storage.
   ///
   /// If [CustomThemes] storage is not found than returns `null`.
-  static T of<T extends CustomThemeData>(BuildContext context) {
+  static T? of<T extends CustomThemeData>(BuildContext context) {
     final customThemes = _of(context);
     if (customThemes == null) {
       return null;
     }
     final ThemeMode mode = customThemes.themeMode ?? ThemeMode.system;
     final Brightness platformBrightness = MediaQuery.platformBrightnessOf(context);
-    T theme;
+    T? theme;
     if (mode == ThemeMode.dark || (mode == ThemeMode.system && platformBrightness == Brightness.dark)) {
       theme = customThemes.darkThemesStorageByType.get<T>();
     }
@@ -31,22 +31,22 @@ class CustomThemes extends InheritedWidget {
         : theme;
   }
 
-  final ThemeMode themeMode;
+  final ThemeMode? themeMode;
 
   /// List of custom themes data.
   final List<CustomThemeData> themes;
 
-  final List<CustomThemeData> darkThemes;
+  final List<CustomThemeData>? darkThemes;
 
   final StorageByTypeImpl themesStorageByType = StorageByTypeImpl();
   final StorageByTypeImpl darkThemesStorageByType = StorageByTypeImpl();
 
   CustomThemes({
-    Key key,
-    this.themes,
+    Key? key,
+    required this.themes,
     this.darkThemes,
     this.themeMode,
-    Widget child,
+    required Widget child,
   })  : super(key: key, child: child) {
     themesStorageByType.setData(themes, recursive: true);
 
--- packages/hy_theme/lib/src/storage_by_type.dart
+++ packages/hy_theme/lib/src/storage_by_type.dart
@@ -1,3 +1,6 @@
+/// Function to get element with type [E] from [storage].
+typedef GetFromSubStorage<E> = E? Function(StorageByType storage);
+
 /// Storage for instances by type.
 abstract class StorageByType {
   /// List of types in storage.
@@ -6,19 +9,20 @@ abstract class StorageByType {
   /// Obtains the instance of given type [E].
   ///
   /// If there is no instance for type, then `null` will be returned.
-  E get<E>();
+  E? get<E>([GetFromSubStorage<E>? func]);
 }
 
 /// Storage for instances by type.
-class StorageByTypeImpl with StorageByTypeMixin {}
+class StorageByTypeImpl<T> with StorageByTypeMixin<T> {}
 
 /// Mixin with functionality of storage for instances by type.
-abstract class StorageByTypeMixin implements StorageByType {
+mixin StorageByTypeMixin<T> implements StorageByType {
   /// Map of instances.
-  final Map<Type, dynamic> _map = {};
-  final List<StorageByType> _subStorage = [];
+  final _map = <Type, T>{};
+  final _subStorage = <StorageByType>[];
 
   /// List of types in storage.
+  @override
   Iterable<Type> get types => _map.keys;
 
   /// Set list of instances.
@@ -27,35 +31,34 @@ abstract class StorageByTypeMixin implements StorageByType {
   /// Also list should not contain null.
   ///
   /// If [recursive] is `true` than
-  void setData(Iterable data, {bool recursive = false}) {
+  void setData(Iterable<T> data, {bool recursive = false}) {
     data.forEach(recursive ? _addRecursive : _addInstance);
   }
 
   /// Obtains the instance of given type [E].
   ///
   /// If there is no instance for type, then `null` will be returned
-  E get<E>() {
-    var res = _map[E];
+  @override
+  E? get<E>([GetFromSubStorage<E>? func]) {
+    Object? res = _map[E];
     if (res == null && _subStorage.isNotEmpty) {
       for (final storage in _subStorage) {
-        res = storage.get<E>();
-        if (res != null) return res;
+        res = func != null ? func.call(storage) : storage.get<E>();
+        if (res != null) return res as E?;
       }
     }
 
-    return res as E;
+    return res as E?;
   }
 
-  void _addInstance(item) {
-    assert(item != null);
-
+  void _addInstance(T item) {
     final type = item.runtimeType;
     assert(_map.containsKey(type) == false);
 
     _map[type] = item;
   }
 
-  void _addRecursive(item) {
+  void _addRecursive(T item) {
     _addInstance(item);
     if (item is StorageByType) {
       _subStorage.add(item);
