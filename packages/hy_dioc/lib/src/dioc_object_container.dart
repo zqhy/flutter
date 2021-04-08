5c24927bd3d2a0f09b7aec54341304199c20e465
innwin <innwin@live.com>
2021-04-08 10:57:11 +0800

    fixed `hy_injector` 容器注册和实体获取丢失类型的问题

--- packages/hy_dioc/lib/src/dioc_object_container.dart
+++ packages/hy_dioc/lib/src/dioc_object_container.dart
@@ -10,18 +10,18 @@ class DiocObjectContainer implements ObjectContainerRegister, ObjectContainer {
   }
 
   @override
-  T get<T>({String name}) => _container.has<T>() ? _container.get(name: name) : null;
+  T get<T>({String name}) => _container.has<T>() ? _container.get<T>(name: name) : null;
 
   @override
-  T getRequired<T>({String name}) => _container.get(name: name);
+  T getRequired<T>({String name}) => _container.get<T>(name: name);
 
   @override
   void register<T>(ObjectCreator<T> creator, {String name}) {
-    _container.register((c) => creator(this), name: name, defaultMode: InjectMode.create);
+    _container.register<T>((c) => creator(this), name: name, defaultMode: InjectMode.create);
   }
 
   @override
   void registerSingleton<T>(ObjectCreator<T> creator, {String name}) {
-    _container.register((c) => creator(this), name: name, defaultMode: InjectMode.singleton);
+    _container.register<T>((c) => creator(this), name: name, defaultMode: InjectMode.singleton);
   }
 }
--- packages/hy_injector/lib/src/injector_object_container.dart
+++ packages/hy_injector/lib/src/injector_object_container.dart
@@ -10,18 +10,18 @@ class InjectorObjectContainer implements ObjectContainerRegister, ObjectContaine
   }
 
   @override
-  T? get<T>({String? name}) => _injector.exists<T>(dependencyName: name ?? "") ? _injector.get(dependencyName: name ?? "") : null;
+  T? get<T>({String? name}) => _injector.exists<T>(dependencyName: name ?? "") ? _injector.get<T>(dependencyName: name ?? "") : null;
 
   @override
-  T getRequired<T>({String? name}) => _injector.get(dependencyName: name ?? "");
+  T getRequired<T>({String? name}) => _injector.get<T>(dependencyName: name ?? "");
 
   @override
   void register<T>(ObjectCreator<T> creator, {String? name}) {
-    _injector.registerDependency(() => creator(this), dependencyName: name ?? "");
+    _injector.registerDependency<T>(() => creator(this), dependencyName: name ?? "");
   }
 
   @override
   void registerSingleton<T>(ObjectCreator<T> creator, {String? name}) {
-    _injector.registerSingleton(() => creator(this), dependencyName: name ?? "");
+    _injector.registerSingleton<T>(() => creator(this), dependencyName: name ?? "");
   }
 }
