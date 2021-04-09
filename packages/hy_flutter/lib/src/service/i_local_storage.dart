35b79212944ebf884721247d936314e18911e98b
innwin <innwin@live.com>
2021-04-09 08:20:15 +0800

    修改 `hy_flutter`中`ILocalStorage`接口

--- packages/hy_flutter/lib/src/service/i_local_storage.dart
+++ packages/hy_flutter/lib/src/service/i_local_storage.dart
@@ -1,7 +1,7 @@
 abstract class ILocalStorage {
   Future<bool> put<T>(String key, T value);
-  T? get<T>(String key, T transformer(Map map), {T? defValue});
-  Map? getObject(String key);
+  T? get<T>(String key, T transformer(dynamic map), {T? defValue});
+  dynamic getJsonObject(String key);
   String? getString(String key, {String? defValue});
   Future<bool> putString(String key, String value);
   bool? getBool(String key, {bool? defValue});
@@ -12,7 +12,6 @@ abstract class ILocalStorage {
   Future<bool> putDouble(String key, double value);
   List<String>? getStringList(String key, {List<String>? defValue = const []});
   Future<bool> putStringList(String key, List<String> value);
-  dynamic getDynamic(String key, {Object? defValue});
   bool haveKey(String key);
   Set<String> getKeys();
   Future<bool> remove(String key);
